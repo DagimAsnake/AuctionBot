e70ef000f384f62e771383001e9946eab7ce20c5
Dagim Asnake <dagimasnake1@gmail.com>
2023-10-13 01:56:25 -1200

    trying to fix the inline keyboard

--- bot.js
+++ bot.js
@@ -1,100 +1,96 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const { Telegraf } = require('telegraf');
-
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const helpMessage = `
     A simple Auction Bot
     /start - Start the bot
-    /help - For contacting us
     /create - For creating your auction
-`
+`;
 
 bot.start((ctx) => {
-    ctx.reply(helpMessage)
-})
+  ctx.reply(helpMessage);
+});
 
-bot.help((ctx) => {
-    bot.telegram.sendMessage(ctx.chat.id, "Help", {
-        reply_markup: {
-          keyboard: [
-            [
-              { text: "Contact" },
-            ],
-            [
-              { text: "Remove Keyboard" },
-            ]
-          ],
-          resize_keyboard: true,
-          one_time_keyboard: true
-        }
-      })
-})
+let name, bid, description, opens, closes;
+let state = 0;
 
-bot.hears('Contact', ctx => {
-    ctx.reply('Contact the owner of the bot  @DagimAsnake');
-  })
+bot.command("create", ctx => {
+  handleInput(ctx);
+});
 
+function handleInput(ctx) {
+  if (state === 0) {
+    ctx.reply("Enter Your product name?");
+    state++;
+  } else if (state === 1) {
+    name = ctx.message.text;
+    ctx.reply("Enter Your product initial bid?");
+    state++;
+  } else if (state === 2) {
+    bid = ctx.message.text;
+    ctx.reply("Enter Your product description?");
+    state++;
+  } else if (state === 3) {
+    description = ctx.message.text;
+    ctx.reply("Enter Your auction opens at?");
+    state++;
+  } else if (state === 4) {
+    opens = ctx.message.text;
+    ctx.reply("Enter Your auction closes at?");
+    state++;
+  } else {
+    closes = ctx.message.text;
+    ctx.reply("Enter Your Product image?");
+    state++;
+  }
+}
 
-  let name, bid,  description, opens, closes
-  let state = 0;
-  
-  bot.command("create", ctx => { 
-    handleInput(ctx); 
-  });
-  
-  function handleInput(ctx) {
-    
-    if(state === 0) {
-      ctx.reply("Enter Your product name?");
-      state++;
-    }
-    else if(state === 1) {
-      name = ctx.message.text;
-      ctx.reply("Enter Your product inital bid?"); 
-      state++;
-    }
-    else if(state === 2) {
-      bid = ctx.message.text;
-      ctx.reply("Enter Your product description?");
-      state++;
-    }
-    else if(state === 3) {
-        description = ctx.message.text;
-        ctx.reply("Enter Your auction opens at?");
-        state++;
-      }
-      else if(state === 4) {
-        opens = ctx.message.text;
-        ctx.reply("Enter Your auction closes at?");
-        state++;
-      }
-    else {
-        closes = ctx.message.text;
-        ctx.reply("Enter Your Product image?");
+bot.on("photo", (ctx) => {
+  if (state === 6) {
+    const photo = ctx.message.photo[0]; // Get the first photo from the message
 
     const last = `
-                  Name: ${name} 
-                  Bid: ${bid}
-                  Description: ${description}
-                  Opens: ${opens}
-                  Closes: ${closes}`;
+    Name: ${name}
+    Bid: ${bid}
+    Description: ${description}
+    Opens: ${opens}
+    Closes: ${closes}
+    `;
 
-        bot.on('photo', (ctx) => {
-          const photo = ctx.message.photo[0] // Get the first photo from the message
-        
-          // Reply with the photo and the text "Hi there" below it
-          ctx.replyWithPhoto(photo.file_id, { caption: `${last}` })
-        })
-        
-      state = 0; 
-    }
-  
-    bot.on('text', ctx => {
-      handleInput(ctx);
-    });
-  
+    // Reply with the photo and the text below it
+    ctx.replyWithPhoto(
+      photo.file_id,
+      {
+        caption: last,
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { text: "Pay", callback_data: "pay" },
+              { text: "Back", callback_data: "back" },
+            ],
+          ],
+        },
+      }
+    );
   }
 
-bot.launch()
+  state = 0
+});
+
+bot.on('text', ctx => {
+  handleInput(ctx);
+  });
+
+bot.action("pay", (ctx) => {
+  ctx.reply("Payment action selected");
+});
+
+bot.action("back", (ctx) => {
+  // Reset the state
+  state = 0;
+  ctx.reply("Action cancelled");
+});
+
+bot.launch();
